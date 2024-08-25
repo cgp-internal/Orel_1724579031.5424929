5bc29ba857050f52e0ffc8767137fff299d2f6d9
cgp-client <code.gen.pro.system@gmail.com>
2024-08-25 12:46:57 +0300

    Update src/models/Room.js

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,23 +1,10 @@
-class Room {
-  constructor(id, name, createdBy) {
-    this.id = id;
-    this.name = name;
-    this.createdBy = createdBy;
-  }
+const mongoose = require("mongoose");
 
-  static async create(db, name, createdBy) {
-    const roomId = await db.run(`INSERT INTO rooms (name, created_by) VALUES (?, ?)`, name, createdBy);
-    return new Room(roomId, name, createdBy);
-  }
+const roomSchema = new mongoose.Schema({
+  name: String,
+  createdBy: String,
+});
 
-  static async getAll(db) {
-    const rooms = await db.all(`SELECT * FROM rooms`);
-    return rooms.map((room) => new Room(room.id, room.name, room.created_by));
-  }
-
-  static async delete(db, id) {
-    await db.run(`DELETE FROM rooms WHERE id = ?`, id);
-  }
-}
+const Room = mongoose.model("Room", roomSchema);
 
 module.exports = { Room };
