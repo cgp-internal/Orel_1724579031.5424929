a1d956dc102fa80e702bcafc4b2bd7392e8a9d21
cgp-client <code.gen.pro.system@gmail.com>
2024-08-25 12:52:48 +0300

    Update src/api/room.js

--- src/api/room.js
+++ src/api/room.js
@@ -1,18 +1,49 @@
-const mongoose = require('mongoose');
+const awsLambda = require('aws-lambda');
 const Room = require('./models/Room');
 
-mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true });
+let db;
 
-async function createRoom(name, createdBy) {
-  return Room.create({ name, createdBy });
+async function connectToDB() {
+  if (!db) {
+    db = await require('mongoose').createConnection(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
+  return db;
 }
 
-async function getRooms() {
+async function createRoom(event, context) {
+  const db = await connectToDB();
+  const room = new Room({ name: event.name, createdBy: event.createdBy });
+  return room.save();
+}
+
+async function getRooms(event, context) {
+  const db = await connectToDB();
   return Room.find().exec();
 }
 
-async function deleteRoom(id) {
-  return Room.findByIdAndRemove(id).exec();
+async function deleteRoom(event, context) {
+  const db = await connectToDB();
+  return Room.findByIdAndRemove(event.id).exec();
 }
 
-module.exports = { createRoom, getRooms, deleteRoom };
+exports.handler = async function(event, context) {
+  switch (event.path) {
+    case '/room':
+      if (event.httpMethod === 'POST') {
+        return createRoom(event, context);
+      }
+      break;
+    case '/rooms':
+      if (event.httpMethod === 'GET') {
+        return getRooms(event, context);
+      }
+      break;
+    case '/room/{id}':
+      if (event.httpMethod === 'DELETE') {
+        return deleteRoom(event, context);
+      }
+      break;
+    default:
+      return { statusCode: 404 };
+  }
+}
