34bf6fba8e61ad4594fa0d8b4ad3d64971ebf86e
cgp-client <code.gen.pro.system@gmail.com>
2024-08-25 12:52:50 +0300

    Update src/services/room.js

--- src/services/room.js
+++ src/services/room.js
@@ -1,17 +1,37 @@
-const { createRoom, getRooms, deleteRoom } = require('./api/room');
+const AWS = require('aws-sdk');
 
 let roomService = {};
 
 roomService.createRoom = async function(name, createdBy) {
-  return createRoom(name, createdBy);
+  const lambda = new AWS.Lambda({ region: 'your-region' });
+  const params = {
+    FunctionName: 'createRoom',
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify({ name, createdBy }),
+  };
+  const response = await lambda.invoke(params).promise();
+  return JSON.parse(response.Payload);
 }
 
 roomService.getRooms = async function() {
-  return getRooms();
+  const lambda = new AWS.Lambda({ region: 'your-region' });
+  const params = {
+    FunctionName: 'getRooms',
+    InvocationType: 'RequestResponse',
+  };
+  const response = await lambda.invoke(params).promise();
+  return JSON.parse(response.Payload);
 }
 
 roomService.deleteRoom = async function(id) {
-  return deleteRoom(id);
+  const lambda = new AWS.Lambda({ region: 'your-region' });
+  const params = {
+    FunctionName: 'deleteRoom',
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify({ id }),
+  };
+  const response = await lambda.invoke(params).promise();
+  return JSON.parse(response.Payload);
 }
 
 module.exports = { createRoom: roomService.createRoom, getRooms: roomService.getRooms, deleteRoom: roomService.deleteRoom };
